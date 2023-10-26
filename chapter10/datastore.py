0a74390725f3e350fd9a215b6cbdf3441d70a477
Euan Walker <40440665@live.napier.ac.uk>
2023-10-26 16:42:46 +0100

    Fixing Bugs Pt2

--- chapter10/datastore.py
+++ chapter10/datastore.py
@@ -20,8 +20,7 @@ def close_db_connection(exception):
 def init_db():
     with app.app_context():
         db = get_db
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
+        with app.open_resource('schema.sql', mode='r') as f: db.cursor().executescript(f.read())
         db.commit()
 
 @app.route("/")
