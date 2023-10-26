36567c0beea3d6a0c66bb6719e207aec63034563
Euan Walker <40440665@live.napier.ac.uk>
2023-10-26 16:46:48 +0100

    Fixing Bugs Pt4

--- chapter10/datastore.py
+++ chapter10/datastore.py
@@ -21,7 +21,7 @@ def init_db():
     with app.app_context():
         db = get_db
         with app.open_resource('schema.sql', mode='r') as f:
- 	 db.cursor().executescript(f.read())
+            db.cursor().executescript(f.read())
         db.commit()
 
 @app.route("/")
